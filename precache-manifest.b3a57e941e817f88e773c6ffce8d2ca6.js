01d6b0c7f29fdd2917456e47493aa8d3fa0247e7
SicknessW2 <sosiska74vip@gmail.com>
2020-09-18 14:49:02 +0500

    Updates

---
+++ precache-manifest.b3a57e941e817f88e773c6ffce8d2ca6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46c432b9cb429bdca32d82d7607fa7c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "60412d98f8767b6c5d73",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "407ed4b96d86303bf44e",
+    "url": "./static/css/main.3fb6893f.chunk.css"
+  },
+  {
+    "revision": "60412d98f8767b6c5d73",
+    "url": "./static/js/2.b9f3d9e3.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.b9f3d9e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "407ed4b96d86303bf44e",
+    "url": "./static/js/main.b19f9d73.chunk.js"
+  },
+  {
+    "revision": "e97a3e10619b3042175f",
+    "url": "./static/js/runtime-main.2be6c2a0.js"
+  },
+  {
+    "revision": "4bb576814cbef79b31fd788e488fe286",
+    "url": "./static/media/banner.4bb57681.jpg"
+  },
+  {
+    "revision": "36c9fda3c7291ed7a3369a7deeabf11b",
+    "url": "./static/media/bg.36c9fda3.jpg"
+  },
+  {
+    "revision": "05de3a777f6afda2a1033307e2cc957c",
+    "url": "./static/media/overlay.05de3a77.png"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
